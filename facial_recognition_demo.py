7930650f36f9b558def430a5d14c7a163ea7f654
Nixon Hanna <nixonhanna95@gmail.com>
2022-02-01 18:24:49 -0600

    Update facial_recognition_demo.py

--- facial_recognition_demo.py
+++ facial_recognition_demo.py
@@ -2,19 +2,10 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
+# Set a variable for the webcam
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
+# Load a sample picture of known people
 nixon_image = face_recognition.load_image_file("nixon.jpg")
 nixon_face_encoding = face_recognition.face_encodings(nixon_image)[0]
 
@@ -23,10 +14,6 @@ shrey_face_encoding = face_recognition.face_encodings(shrey_image)[0]
 
 christina_image = face_recognition.load_image_file("christina.jpg")
 christina_face_encoding = face_recognition.face_encodings(christina_image)[0]
-
-# Load a second sample picture and learn how to recognize it.
-# biden_image = face_recognition.load_image_file("biden.jpg")
-# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
@@ -69,12 +56,7 @@ while True:
             matches = face_recognition.compare_faces(
                 known_face_encodings, face_encoding)
             name = "Unknown"
-
-        # # If a match was found in known_face_encodings, just use the first one.
-        # if True in matches:
-        #     first_match_index = matches.index(True)
-        #     name = known_face_names[first_match_index]
-
+            
         # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(
                 known_face_encodings, face_encoding)
