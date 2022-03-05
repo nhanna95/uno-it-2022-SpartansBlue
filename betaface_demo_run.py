bf845be001c92449d9bced4eea0d3380f2282adf
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-05 15:59:43 -0600

    testing stuffs

--- betaface_demo_run.py
+++ betaface_demo_run.py
@@ -3,10 +3,11 @@ import json
 
 f = faceData()
 
-data = f.getData(filename="sample.png", url="http://betafaceapi.com/api_examples/sample.png")
+data = f.getData(filename="sample.png", url="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/famous-hispanic-people-aexandria-ocasio-cortez-1598643634.jpg")
 
 data2 = json.dumps(data)
 parsed = json.loads(data2)
-print(json.dumps(parsed))
+
+print(json.dumps(parsed, indent=4, sort_keys=True))
 
 ##print(data)
