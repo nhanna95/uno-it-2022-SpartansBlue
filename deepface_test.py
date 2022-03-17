b642ed397a95883c2c730292c5ebd297da4f0ae2
ShreyAgarwal310 <66689054+ShreyAgarwal310@users.noreply.github.com>
2022-03-17 11:01:07 -0500

    Update deepface_test.py

--- deepface_test.py
+++ deepface_test.py
@@ -1,5 +1,7 @@
 from deepface import DeepFace
 
-obj = DeepFace.analyze(img_path = "shrey.jpg", actions = ['race'])
+obj = DeepFace.analyze(img_path = "shrey.jpeg", actions = ['race'])
 
 print(obj)
+
+#possible races: asian, indian, black, white, middle eastern, latino hispanic
